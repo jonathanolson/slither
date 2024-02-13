513771bc03c47182df413f89ecd0ab3b6fa64d7d
Jonathan Olson <jonathan.olson@colorado.edu>
2024-02-12 22:04:29 -0700

    Improved tesseract options

--- src/scan/tesseractTest.ts
+++ src/scan/tesseractTest.ts
@@ -1,10 +1,20 @@
 
-import stuff from 'tesseract.js/dist/tesseract.esm.min.js';
+import tesseract from 'tesseract.js/dist/tesseract.esm.min.js';
 
-const createWorker = stuff.createWorker;
+const createWorker = tesseract.createWorker;
+
+// TODO: scheduler, see https://github.com/naptha/tesseract.js/blob/HEAD/docs/workers_vs_schedulers.md
+
+// TODO: with multiple rectangles, see https://github.com/naptha/tesseract.js/blob/HEAD/docs/examples.md
+// TODO: for ideal, with multiple workers for the multiple rectangles
 
 export default async ( url: string ) => {
   const worker = await createWorker( 'eng' );
+  worker.setParameters( {
+    tessedit_char_whitelist: '0123',
+    tessedit_pageseg_mode: tesseract.PSM.PSM_SPARSE_TEXT
+  } );
+
   const ret = await worker.recognize( url );
   const text = ret.data.text;
   await worker.terminate();
