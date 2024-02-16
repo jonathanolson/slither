f473c41d9bfeedcb525e52df68219ff2542a735d
Jonathan Olson <jonathan.olson@colorado.edu>
2024-02-16 15:08:08 -0700

    Move PuzzleContainerNode

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import scanURL from './scan/scanURL.ts';
 import SlitherQueryParameters from './SlitherQueryParameters.ts';
 import PuzzleNode from './view/PuzzleNode.ts';
 import { Shape } from 'phet-lib/kite';
+import PuzzleContainerNode from './view/PuzzleContainerNode.ts';
 
 // @ts-ignore
 window.assertions.enableAssert();
@@ -46,104 +47,7 @@ window.oncontextmenu = e => e.preventDefault();
 
 const layoutBoundsProperty = new Property( new Bounds2( 0, 0, window.innerWidth, window.innerHeight ) );
 
-class PuzzleContainer extends Sizable( Node ) {
-
-  private backgroundRect: Rectangle;
-  private puzzleWrapper: Node;
-  private puzzleNode: PuzzleNode | null = null;
-  private zoomListener: AnimatedPanZoomListener;
-
-  public constructor() {
-    // TODO: isolate out these options
-    super( {
-      layoutOptions: {
-        stretch: true,
-        grow: 1
-      }
-    } );
-
-    this.backgroundRect = new Rectangle( {
-      fill: '#ccc'
-    } );
-    this.addChild( this.backgroundRect );
-
-    this.puzzleWrapper = new Node();
-    this.zoomListener = new AnimatedPanZoomListener( this.puzzleWrapper, {
-      maxScale: 10
-    } );
-    // display.addInputListener( this.zoomListener );
-    this.addInputListener( this.zoomListener );
-
-    this.addChild( this.puzzleWrapper );
-
-    const layoutListener = this.updateLayout.bind( this );
-    this.localPreferredWidthProperty.lazyLink( layoutListener );
-    this.localPreferredHeightProperty.lazyLink( layoutListener );
-
-    this.updateLayout();
-  }
-
-  public step( dt: number ): void {
-    this.zoomListener.step( dt );
-  }
-
-  private updateLayout(): void {
-    const width = this.localPreferredWidth;
-    const height = this.localPreferredHeight;
-
-    if ( width !== null ) {
-      this.backgroundRect.rectWidth = width;
-    }
-    if ( height !== null ) {
-      this.backgroundRect.rectHeight = height;
-    }
-
-    if ( this.puzzleNode ) {
-      this.updatePuzzleNodeLayout( this.puzzleNode );
-    }
-
-    if ( width !== null && height !== null && width > 0 && height > 0 ) {
-      const bounds = new Bounds2( 0, 0, width, height );
-
-      this.clipArea = Shape.bounds( bounds );
-
-      // zoomListener.setTargetScale( scale );
-      // TODO: we're getting weird oscillation with this, but figure it out
-      // this.zoomListener.setTargetBounds( this.puzzleNode ? this.puzzleNode.bounds : bounds );
-      this.zoomListener.setTargetBounds( bounds );
-      this.zoomListener.setPanBounds( bounds );
-    }
-  }
-
-  private updatePuzzleNodeLayout( puzzleNode: PuzzleNode ): void {
-    const width = this.localPreferredWidth;
-    const height = this.localPreferredHeight;
-
-    if ( width !== null && height !== null && width > 0 && height > 0 ) {
-      const padding = 20;
-
-      const availableWidth = width - padding * 2;
-      const availableHeight = height - padding * 2;
-
-      const puzzleWidth = puzzleNode.localBounds.width;
-      const puzzleHeight = puzzleNode.localBounds.height;
-
-      const scale = Math.min( availableWidth / puzzleWidth, availableHeight / puzzleHeight );
-
-      puzzleNode.setScaleMagnitude( scale );
-      puzzleNode.center = new Vector2( width / 2, height / 2 );
-    }
-  }
-
-  public setPuzzleNode( puzzleNode: PuzzleNode ): void {
-    this.puzzleNode = puzzleNode;
-    // Update before children, so we don't mess with layout
-    this.updatePuzzleNodeLayout( puzzleNode );
-    this.puzzleWrapper.children = [ puzzleNode ];
-  }
-}
-
-const puzzleNodeContainer = new PuzzleContainer();
+const puzzleContainerNode = new PuzzleContainerNode();
 
 const mainBox = new VBox( {
   children: [
@@ -165,13 +69,13 @@ const mainBox = new VBox( {
             // TODO: UI change while working?
             const puzzle = await scanURL( url );
 
-            puzzleNodeContainer.setPuzzleNode( new PuzzleNode( puzzle ) );
+            puzzleContainerNode.setPuzzleNode( new PuzzleNode( puzzle ) );
           }
         }
         input.click();
       }
     } ),
-    puzzleNodeContainer
+    puzzleContainerNode
   ]
 } );
 layoutBoundsProperty.lazyLink( bounds => {
@@ -209,5 +113,5 @@ display.updateOnRequestAnimationFrame( dt => {
     resize();
   }
 
-  puzzleNodeContainer.step( dt );
+  puzzleContainerNode.step( dt );
 } );
---
+++ src/view/PuzzleContainerNode.ts
@@ -0,0 +1,101 @@
+import { AnimatedPanZoomListener, Node, Rectangle, Sizable } from "phet-lib/scenery";
+import PuzzleNode from "./PuzzleNode";
+import { Bounds2, Vector2 } from "phet-lib/dot";
+import { Shape } from "phet-lib/kite";
+
+export default class PuzzleContainerNode extends Sizable( Node ) {
+
+  private backgroundRect: Rectangle;
+  private puzzleWrapper: Node;
+  private puzzleNode: PuzzleNode | null = null;
+  private zoomListener: AnimatedPanZoomListener;
+
+  public constructor() {
+    // TODO: isolate out these options
+    super( {
+      layoutOptions: {
+        stretch: true,
+        grow: 1
+      }
+    } );
+
+    this.backgroundRect = new Rectangle( {
+      fill: '#ccc'
+    } );
+    this.addChild( this.backgroundRect );
+
+    this.puzzleWrapper = new Node();
+    this.zoomListener = new AnimatedPanZoomListener( this.puzzleWrapper, {
+      maxScale: 10
+    } );
+    // display.addInputListener( this.zoomListener );
+    this.addInputListener( this.zoomListener );
+
+    this.addChild( this.puzzleWrapper );
+
+    const layoutListener = this.updateLayout.bind( this );
+    this.localPreferredWidthProperty.lazyLink( layoutListener );
+    this.localPreferredHeightProperty.lazyLink( layoutListener );
+
+    this.updateLayout();
+  }
+
+  public step( dt: number ): void {
+    this.zoomListener.step( dt );
+  }
+
+  private updateLayout(): void {
+    const width = this.localPreferredWidth;
+    const height = this.localPreferredHeight;
+
+    if ( width !== null ) {
+      this.backgroundRect.rectWidth = width;
+    }
+    if ( height !== null ) {
+      this.backgroundRect.rectHeight = height;
+    }
+
+    if ( this.puzzleNode ) {
+      this.updatePuzzleNodeLayout( this.puzzleNode );
+    }
+
+    if ( width !== null && height !== null && width > 0 && height > 0 ) {
+      const bounds = new Bounds2( 0, 0, width, height );
+
+      this.clipArea = Shape.bounds( bounds );
+
+      // zoomListener.setTargetScale( scale );
+      // TODO: we're getting weird oscillation with this, but figure it out
+      // this.zoomListener.setTargetBounds( this.puzzleNode ? this.puzzleNode.bounds : bounds );
+      this.zoomListener.setTargetBounds( bounds );
+      this.zoomListener.setPanBounds( bounds );
+    }
+  }
+
+  private updatePuzzleNodeLayout( puzzleNode: PuzzleNode ): void {
+    const width = this.localPreferredWidth;
+    const height = this.localPreferredHeight;
+
+    if ( width !== null && height !== null && width > 0 && height > 0 ) {
+      const padding = 20;
+
+      const availableWidth = width - padding * 2;
+      const availableHeight = height - padding * 2;
+
+      const puzzleWidth = puzzleNode.localBounds.width;
+      const puzzleHeight = puzzleNode.localBounds.height;
+
+      const scale = Math.min( availableWidth / puzzleWidth, availableHeight / puzzleHeight );
+
+      puzzleNode.setScaleMagnitude( scale );
+      puzzleNode.center = new Vector2( width / 2, height / 2 );
+    }
+  }
+
+  public setPuzzleNode( puzzleNode: PuzzleNode ): void {
+    this.puzzleNode = puzzleNode;
+    // Update before children, so we don't mess with layout
+    this.updatePuzzleNodeLayout( puzzleNode );
+    this.puzzleWrapper.children = [ puzzleNode ];
+  }
+}
