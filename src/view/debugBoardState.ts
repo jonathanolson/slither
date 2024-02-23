7eca82e3352d7675cdc7eed5f2f43a6b17d7f7c5
Jonathan Olson <jonathan.olson@colorado.edu>
2024-02-23 14:48:28 -0700

    cleanup

--- src/view/debugBoardState.ts
+++ src/view/debugBoardState.ts
@@ -1,17 +1,15 @@
 import { TState } from '../model/data/core/TState.ts';
 import { TBoard } from '../model/board/core/TBoard.ts';
 import { TCompleteData } from '../model/data/combined/TCompleteData.ts';
-import PuzzleModel from '../model/puzzle/PuzzleModel.ts';
 import { TinyProperty } from 'phet-lib/axon';
-import PuzzleModelNode from './PuzzleModelNode.ts';
 import { glassPane } from './glassPane.ts';
 import { NodeOptions } from 'phet-lib/scenery';
+import PuzzleNode from './PuzzleNode.ts';
 
 export const debugBoardState = ( board: TBoard, state: TState<TCompleteData>, options?: NodeOptions ) => {
-  const puzzleModel = new PuzzleModel( {
+  const puzzleNode = new PuzzleNode( {
     board: board,
     stateProperty: new TinyProperty( state )
-  } );
-  const puzzleNode = new PuzzleModelNode( puzzleModel, options );
+  }, options );
   glassPane.addChild( puzzleNode );
 };
