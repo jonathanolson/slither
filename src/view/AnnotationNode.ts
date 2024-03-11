04da5616cf91929c10cb671935bd09164fa8e457
Jonathan Olson <jonathan.olson@colorado.edu>
2024-03-10 18:25:58 -0600

    more views for annotations

--- src/view/AnnotationNode.ts
+++ src/view/AnnotationNode.ts
@@ -9,6 +9,8 @@ export class AnnotationNode extends Node {
   ) {
     let children: Node[];
 
+    console.log( annotation.type );
+
     const getEdgeOutlineShape = ( edge: TEdge ) => {
       const initialShape = new Shape().moveToPoint( edge.start.viewCoordinates ).lineToPoint( edge.end.viewCoordinates );
       const strokedShape = initialShape.getStrokedShape( new LineStyles( {
@@ -57,6 +59,75 @@ export class AnnotationNode extends Node {
         ...annotation.whiteEdges.map( edge => getEdgeColoredOutline( edge, 'red' ) ),
         ...annotation.redEdges.map( edge => getEdgeColoredOutline( edge, 'blue' ) )
       ];
+    }
+    else if ( annotation.type === 'ForcedSolveLoop' ) {
+      children = [
+        ...annotation.regionEdges.map( edge => getEdgeColoredOutline( edge, 'blue' ) ),
+        ...annotation.pathEdges.map( edge => getEdgeColoredOutline( edge, 'red' ) )
+      ];
+    }
+    else if ( annotation.type === 'PrematureForcedLoop' ) {
+      children = [
+        ...annotation.regionEdges.map( edge => getEdgeColoredOutline( edge, 'blue' ) ),
+        ...annotation.pathEdges.map( edge => getEdgeColoredOutline( edge, 'red' ) )
+      ];
+    }
+    else if ( annotation.type === 'CompletingEdgesAfterSolve' ) {
+      children = [
+        ...annotation.whiteEdges.map( edge => getEdgeColoredOutline( edge, 'red' ) ),
+      ];
+    }
+    else if ( annotation.type === 'FaceColoringBlackEdge' ) {
+      children = [
+        getEdgeColoredOutline( annotation.edge, 'black' ),
+      ];
+    }
+    else if ( annotation.type === 'FaceColoringRedEdge' ) {
+      children = [
+        getEdgeColoredOutline( annotation.edge, 'black' ),
+      ];
+    }
+    else if ( annotation.type === 'FaceColorToBlack' ) {
+      children = [
+        getEdgeColoredOutline( annotation.edge, 'black' ),
+      ];
+    }
+    else if ( annotation.type === 'FaceColorToRed' ) {
+      children = [
+        getEdgeColoredOutline( annotation.edge, 'black' ),
+      ];
+    }
+    else if ( annotation.type === 'FaceColorNoTrivialLoop' ) {
+      children = [
+        ...annotation.face.edges.map( edge => getEdgeColoredOutline( edge, 'red' ) ),
+      ];
+    }
+    else if ( annotation.type === 'FaceColorMatchToRed' || annotation.type === 'FaceColorMatchToBlack' || annotation.type === 'FaceColorBalance' || annotation.type === 'FaceColorOneConstrained' ) {
+      children = [
+        ...annotation.balancedPairs.flatMap( ( balancedPair, i ) => {
+          const mainColor = [ 'green', 'blue', 'black' ][ i % 3 ];
+          const oppositeColor = [ 'magenta', 'orange', 'yellow' ][ i % 3 ];
+
+          return [
+            ...balancedPair[ 0 ].map( edge => getEdgeColoredOutline( edge, mainColor ) ),
+            ...balancedPair[ 1 ].map( edge => getEdgeColoredOutline( edge, oppositeColor ) ),
+          ];
+        } )
+      ];
+
+      if ( annotation.type === 'FaceColorMatchToRed' ) {
+        children.push( ...annotation.matchingEdges.map( edge => getEdgeColoredOutline( edge, 'red' ) ) );
+      }
+      else if ( annotation.type === 'FaceColorMatchToBlack' ) {
+        children.push( ...annotation.matchingEdges.map( edge => getEdgeColoredOutline( edge, 'red' ) ) );
+      }
+      else if ( annotation.type === 'FaceColorBalance' ) {
+        children.push( ...annotation.matchingEdges.map( edge => getEdgeColoredOutline( edge, 'magenta' ) ) );
+        children.push( ...annotation.oppositeEdges.map( edge => getEdgeColoredOutline( edge, 'red' ) ) );
+      }
+      else if ( annotation.type === 'FaceColorOneConstrained' ) {
+        children.push( ...annotation.edges.map( edge => getEdgeColoredOutline( edge, 'red' ) ) );
+      }
     }
     else {
       children = [];
