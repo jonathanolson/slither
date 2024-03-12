d159a0c96b6a192fd0b931acd72e8c837ed6a2f7
Jonathan Olson <jonathan.olson@colorado.edu>
2024-03-12 09:24:38 -0600

    adding VertexState object

---
+++ src/model/data/sector/getSectorsFromVertex.ts
@@ -0,0 +1,6 @@
+import { TSector } from './TSector.ts';
+import { TVertex } from '../../board/core/TVertex.ts';
+
+export const getSectorsFromVertex = ( vertex: TVertex ): TSector[] => {
+  return vertex.incomingHalfEdges;
+};
---
+++ src/model/data/vertex/VertexState.ts
@@ -0,0 +1,148 @@
+import { TVertex } from '../../board/core/TVertex.ts';
+import _ from '../../../workarounds/_.ts';
+import { TEdge } from '../../board/core/TEdge.ts';
+import assert, { assertEnabled } from '../../../workarounds/assert.ts';
+import { TSectorData } from '../sector/TSectorData.ts';
+import { getSectorsFromVertex } from '../sector/getSectorsFromVertex.ts';
+
+export class VertexState {
+
+  public readonly order: number;
+  public readonly caseCount: number;
+
+  private readonly matrix: boolean[]; // TODO: bitpacking?
+
+  public constructor(
+    public readonly vertex: TVertex,
+    matrix?: boolean[],
+    caseCount?: number
+  ) {
+    this.order = vertex.edges.length;
+
+    if ( matrix ) {
+      this.matrix = matrix;
+    }
+    else {
+      this.matrix = _.range( 0, this.order * ( this.order - 1 ) + 1 ).map( () => true );
+    }
+
+    if ( caseCount !== undefined ) {
+      this.caseCount = caseCount;
+    }
+    else {
+      this.caseCount = this.matrix.filter( x => x ).length;
+    }
+
+    assertEnabled() && assert( this.matrix.length === this.order * ( this.order - 1 ) + 1 );
+    assertEnabled() && assert( this.caseCount === this.matrix.filter( x => x ).length );
+  }
+
+  public allowsEmpty(): boolean {
+    return this.matrix[ this.order * ( this.order - 1 ) ];
+  }
+
+  public allowsPair( edgeA: TEdge, edgeB: TEdge ): boolean {
+    return this.matrix[ this.getPairIndex( edgeA, edgeB ) ];
+  }
+
+  public getPairIndex( edgeA: TEdge, edgeB: TEdge ): number {
+    const indexA = this.vertex.edges.indexOf( edgeA );
+    const indexB = this.vertex.edges.indexOf( edgeB );
+
+    const minIndex = Math.min( indexA, indexB );
+    const maxIndex = Math.max( indexA, indexB );
+
+    return VertexState.getIndex( minIndex, maxIndex, this.order );
+  }
+
+  public equals( other: VertexState ): boolean {
+    return this.vertex === other.vertex && this.matrix.every( ( x, i ) => x === other.matrix[ i ] );
+  }
+
+  public and( other: VertexState ): VertexState {
+    assertEnabled() && assert( this.vertex === other.vertex );
+
+    return new VertexState( this.vertex, this.matrix.map( ( x, i ) => x && other.matrix[ i ] ) );
+  }
+
+  public or( other: VertexState ): VertexState {
+    assertEnabled() && assert( this.vertex === other.vertex );
+
+    return new VertexState( this.vertex, this.matrix.map( ( x, i ) => x || other.matrix[ i ] ) );
+  }
+
+  public isSubsetOf( other: VertexState ): boolean {
+    return this.matrix.every( ( x, i ) => !x || other.matrix[ i ] );
+  }
+
+  public withEmpty( empty: boolean ): VertexState {
+    return new VertexState( this.vertex, this.matrix.slice( 0, -1 ).concat( empty ) );
+  }
+
+  public withPair( edgeA: TEdge, edgeB: TEdge, pair: boolean ): VertexState {
+    const index = this.getPairIndex( edgeA, edgeB );
+
+    return new VertexState( this.vertex, this.matrix.slice( 0, index ).concat( pair, this.matrix.slice( index + 1 ) ) );
+  }
+
+  public static getIndex( minIndex: number, maxIndex: number, order: number ): number {
+    // upper-triangular matrix indexing
+    return ( minIndex * ( 2 * order - minIndex - 1 ) / 2 ) + ( maxIndex - minIndex - 1 );
+  }
+
+  public static fromLookup( vertex: TVertex, lookup: ( a: TEdge, b: TEdge ) => boolean, allowEmpty: boolean ): VertexState {
+    const order = vertex.edges.length;
+    const matrix: boolean[] = [];
+
+    for ( let i = 0; i < order; i++ ) {
+      for ( let j = i + 1; j < order; j++ ) {
+        matrix.push( lookup( vertex.edges[ i ], vertex.edges[ j ] ) );
+      }
+    }
+    matrix.push( allowEmpty );
+
+    return new VertexState( vertex, matrix );
+  }
+
+  public static withOnlyEmpty( vertex: TVertex ): VertexState {
+    return VertexState.fromLookup( vertex, () => false, true );
+  }
+
+  public static withOnlyPair( vertex: TVertex, edgeA: TEdge, edgeB: TEdge ): VertexState {
+    return VertexState.fromLookup( vertex, ( a, b ) => ( a === edgeA && b === edgeB ) || ( a === edgeB && b === edgeA ), false );
+  }
+
+  public static withoutEmpty( vertex: TVertex ): VertexState {
+    return VertexState.fromLookup( vertex, () => true, false );
+  }
+
+  public static withoutPair( vertex: TVertex, edgeA: TEdge, edgeB: TEdge ): VertexState {
+    return VertexState.fromLookup( vertex, ( a, b ) => ( a !== edgeA && a !== edgeB ) || ( b !== edgeA && b !== edgeB ), true );
+  }
+
+  public static fromVertexSectorData( vertex: TVertex, sectorData: TSectorData ): VertexState {
+    const order = vertex.edges.length;
+    const matrix: boolean[] = [];
+
+    const sectors = getSectorsFromVertex( vertex );
+    const sectorStates = sectors.map( sector => sectorData.getSectorState( sector ) );
+
+    for ( let i = 0; i < order; i++ ) {
+      const edgeA = vertex.edges[ i ];
+      for ( let j = i + 1; j < order; j++ ) {
+        const edgeB = vertex.edges[ j ];
+
+        matrix.push( sectorStates.every( ( state, i ) => {
+          const sector = sectors[ i ];
+          let count = 0;
+          if ( edgeA === sector.edge || edgeA === sector.next.edge ) { count++; }
+          if ( edgeB === sector.edge || edgeB === sector.next.edge ) { count++; }
+          return state.allows( count );
+        } ) );
+      }
+    }
+    matrix.push( sectorStates.every( state => state.zero ) );
+
+    return new VertexState( vertex, matrix );
+  }
+}
