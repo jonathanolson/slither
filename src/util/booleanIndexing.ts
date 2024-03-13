132168130f4b44483755c0838506d68c710e2438
Jonathan Olson <jonathan.olson@colorado.edu>
2024-03-13 16:26:25 -0600

    adding booleanIndexing

---
+++ src/util/booleanIndexing.ts
@@ -0,0 +1,55 @@
+import _ from '../workarounds/_.ts';
+
+const lazyFactorials: number[] = [];
+
+export const getLazyFactorials = ( n: number ): number[] => {
+  while ( lazyFactorials.length <= n ) {
+    lazyFactorials.push( lazyFactorials.length === 0 ? 1 : lazyFactorials[ lazyFactorials.length - 1 ] * lazyFactorials.length );
+  }
+
+  return lazyFactorials;
+};
+
+export const getBinomialCoefficient = ( n: number, k: number, factorials: number[] ): number => {
+  return factorials[ n ] / ( factorials[ k ] * factorials[ n - k ] );
+};
+
+export const getCombinationIndex = ( combination: number[], collectionSize: number ): number => {
+
+  // be paranoid about sorted inputs
+  combination = _.sortBy( combination );
+
+  const factorials = getLazyFactorials( collectionSize );
+
+  let index = 0;
+  const M = combination.length;
+
+  for ( let i = 0; i < M; i++ ) {
+    const a = combination[ i - 1 ] + 1;
+    const b = combination[ i ];
+
+    for ( let j = ( i == 0 ? 0 : a ); j < b; j++ ) {
+      index += getBinomialCoefficient( collectionSize - j - 1, M - i - 1, factorials );
+    }
+  }
+
+  return index;
+};
+
+export const getCombinationQuantity = ( collectionSize: number, combinationSize: number ): number => {
+  return getBinomialCoefficient( collectionSize, combinationSize, getLazyFactorials( collectionSize ) );
+};
+
+export const getBinaryIndex = ( combination: number[], collectionSize: number ): number => {
+  let index = 0;
+
+  for ( let i = 0; i < combination.length; i++ ) {
+    index += 1 << ( collectionSize - combination[ i ] - 1 );
+  }
+
+  return index;
+};
+
+export const getBinaryQuantity = ( collectionSize: number ): number => {
+  return 1 << collectionSize;
+};
