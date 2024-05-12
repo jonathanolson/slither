bc4cdcfa007638239e8554594601110d4a356b4c
Jonathan Olson <jonathan.olson@colorado.edu>
2024-05-12 14:02:23 -0600

    Using BinaryRuleCollection (initial)

--- src/model/pattern/BinaryRuleCollection.ts
+++ src/model/pattern/BinaryRuleCollection.ts
@@ -2,13 +2,74 @@ import { TPatternBoard } from './TPatternBoard.ts';
 import { serializePatternBoard } from './serializePatternBoard.ts';
 import { compressByteArray, decompressByteArray } from '../../util/compression.ts';
 import { deserializePatternBoard } from './deserializePatternBoard.ts';
+import assert, { assertEnabled } from '../../workarounds/assert.ts';
+import { PatternRule } from './PatternRule.ts';
+import _ from '../../workarounds/_.ts';
 
 export class BinaryRuleCollection {
+
+  public readonly ruleIndices: number[] = [];
+
   public constructor(
     public readonly patternBoards: TPatternBoard[],
     public readonly data: Uint8Array,
     public readonly highlander: boolean,
-  ) {}
+  ) {
+    let index = 0;
+    while ( index < data.length ) {
+      this.ruleIndices.push( index );
+
+      let patternEndCount = 0;
+      while ( patternEndCount < 2 ) {
+        assertEnabled() && assert( index < data.length, 'Unexpected end of data' );
+
+        if ( data[ index++ ] === 0xff ) {
+          patternEndCount++;
+        }
+      }
+    }
+  }
+
+  public getRules(): PatternRule[] {
+    const rules: PatternRule[] = [];
+
+    this.forEachRule( rule => rules.push( rule ) );
+
+    return rules;
+  }
+
+  public get size(): number {
+    return this.ruleIndices.length;
+  }
+
+  public getRule( index: number ): PatternRule {
+    return PatternRule.fromBinary( this.patternBoards, this.data, this.ruleIndices[ index ], this.highlander ).rule;
+  }
+
+  public forEachRule( callback: ( rule: PatternRule ) => void ): void {
+    for ( let i = 0; i < this.ruleIndices.length; i++ ) {
+      callback( this.getRule( i ) );
+    }
+  }
+
+  public withPatternBoardFilter( patternBoardFilter: ( patternBoard: TPatternBoard ) => boolean ): BinaryRuleCollection {
+    const includedMap = this.patternBoards.map( patternBoardFilter );
+    const filteredPatternBoards = this.patternBoards.filter( ( _, i ) => includedMap[ i ] );
+
+    const bytes: number[] = [];
+
+    for ( let i = 0; i < this.ruleIndices.length; i++ ) {
+      const startIndex = this.ruleIndices[ i ];
+      const patternBoardIndex = this.data[ startIndex ];
+
+      if ( includedMap[ patternBoardIndex ] ) {
+        const endIndex = i + 1 < this.ruleIndices.length ? this.ruleIndices[ i + 1 ] : this.data.length;
+        bytes.push( ...this.data.slice( startIndex, endIndex ) );
+      }
+    }
+
+    return new BinaryRuleCollection( filteredPatternBoards, new Uint8Array( bytes ), this.highlander );
+  }
 
   public serialize(): SerializedBinaryRuleCollection {
     return {
@@ -29,6 +90,23 @@ export class BinaryRuleCollection {
       decompressed,
       serialized.highlander,
     );
+  }
+
+  public static empty(): BinaryRuleCollection {
+    return new BinaryRuleCollection( [], new Uint8Array( 0 ), false );
+  }
+
+  public static fromRules( rules: PatternRule[] ): BinaryRuleCollection {
+    const isHighlander = rules.some( rule => rule.highlander );
+    const patternBoards = _.uniq( rules.map( rule => rule.patternBoard ) );
+
+    const bytes: number[] = [];
+
+    for ( const rule of rules ) {
+      bytes.push( ...rule.getBinary( patternBoards ) );
+    }
+
+    return new BinaryRuleCollection( patternBoards, new Uint8Array( bytes ), isHighlander );
   }
 }
 
--- src/model/solver/patternSolverFactory.ts
+++ src/model/solver/patternSolverFactory.ts
@@ -3,15 +3,15 @@ import { TState } from '../data/core/TState.ts';
 import { TCompleteData } from '../data/combined/TCompleteData.ts';
 import { CompositeSolver } from './CompositeSolver.ts';
 import { TAnnotatedAction } from '../data/core/TAnnotatedAction.ts';
-import { getAll10Edge20HighlanderCollection } from '../pattern/data/getAll10Edge20HighlanderCollection.ts';
 import { ScanPatternSolver } from './ScanPatternSolver.ts';
 import { curatedRules } from '../pattern/data/curatedRules.ts';
 import { standardSolverFactory } from './standardSolverFactory.ts';
 import { BoardPatternBoard } from '../pattern/BoardPatternBoard.ts';
 import { getEmbeddings } from '../pattern/getEmbeddings.ts';
-import { PatternRuleCollection } from '../pattern/PatternRuleCollection.ts';
+import { BinaryRuleCollection } from '../pattern/BinaryRuleCollection.ts';
+import allBinaryData from '../../../data-collections/binary-all-10-edge-20-highlander.json';
 
-let embeddableCollection: PatternRuleCollection = PatternRuleCollection.empty();
+let embeddableCollection: BinaryRuleCollection = BinaryRuleCollection.empty();
 let lastBoard: TBoard | null = null;
 let lastBoardPatternBoard: BoardPatternBoard | null = null;
 
@@ -21,22 +21,11 @@ export const patternSolverFactory = ( board: TBoard, state: TState<TCompleteData
     lastBoard = board;
     lastBoardPatternBoard = new BoardPatternBoard( board );
 
-    const collection = getAll10Edge20HighlanderCollection();
+    const binaryCollection = BinaryRuleCollection.deserialize( allBinaryData );
 
-    const bytes: number[] = [];
-
-    embeddableCollection = PatternRuleCollection.empty();
-    collection.forEachRule( rule => {
-        bytes.push( ...rule.getBinary( collection.patternBoards ) );
-
-      // TODO: HOW CAN WE CACHE THIS, it might memory leak getEmbeddings?
-      // TODO: We can side-step this and NOT use getEmbeddings(!)
-      if ( getEmbeddings( rule.patternBoard, lastBoardPatternBoard! ).length > 0 ) {
-        embeddableCollection.addRule( rule );
-      }
+    embeddableCollection = binaryCollection.withPatternBoardFilter( patternBoard => {
+      return getEmbeddings( patternBoard, lastBoardPatternBoard! ).length > 0;
     } );
-
-    debugger;
   }
 
   const boardPatternBoard = lastBoardPatternBoard!;
