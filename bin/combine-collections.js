6a27a9e8fccba743aa940920613a2c8726c770c0
Jonathan Olson <jonathan.olson@colorado.edu>
2024-05-22 20:52:03 -0600

    Omg, attempts

--- bin/combine-collections.js
+++ bin/combine-collections.js
@@ -7,6 +7,8 @@ const lockfileOptions = {
   stale: 60000,
 };
 
+const name = process.argv[ 2 ];
+
 os.setPriority( os.constants.priority.PRIORITY_LOW );
 
 ( async () => {
@@ -74,106 +76,118 @@ os.setPriority( os.constants.priority.PRIORITY_LOW );
   // TODO: EEEEK highlander has ISSUES
 
 
-  await writeWith( 'square-only-edge',
-    [
-      'square-only-edge-unrestricted',
-    ],
-    [
-      'square-only-edge',
-    ]
-  );
+  if ( name === 'square-only-edge' ) {
+    await writeWith( 'square-only-edge',
+      [
+        'square-only-edge-unrestricted',
+      ],
+      [
+        'square-only-edge',
+      ]
+    );
+  }
+  else if ( name === 'square-only-color' ) {
+    await writeWith( 'square-only-color',
+      [
+        'square-only-color-unrestricted',
+      ],
+      [
+        // 'square-only-color', TODO: nope, BAD!
+      ]
+    );
+  }
+  else if ( name === 'square-only-edge-sector' ) {
+    await writeWith( 'square-only-edge-sector',
+      [
+        'square-only-edge-sector-unrestricted',
+        'square-only-edge-unrestricted',
+      ],
+      [
+        'square-only-edge-sector',
+        'square-only-edge',
+      ]
+    );
+  }
+  else if ( name === 'square-only-all' ) {
+    await writeWith( 'square-only-all',
+      [
+        'square-only-all-unrestricted',
+        'square-only-color-unrestricted',
+        'square-only-edge-sector-unrestricted',
+        'square-only-edge-unrestricted',
+      ],
+      [
+        'square-only-all',
+        'square-only-edge-sector',
+        'square-only-edge',
+      ]
+    );
+  }
+  else if ( name === 'general-edge' ) {
+    await writeWith( 'general-edge',
 
-  await writeWith( 'square-only-color',
-    [
-      'square-only-color-unrestricted',
-    ],
-    [
-      // 'square-only-color', TODO: nope, BAD!
-    ]
-  );
-
-  await writeWith( 'square-only-edge-sector',
-    [
-      'square-only-edge-sector-unrestricted',
-      'square-only-edge-unrestricted',
-    ],
-    [
-      'square-only-edge-sector',
-      'square-only-edge',
-    ]
-  );
-
-  await writeWith( 'square-only-all',
-    [
-      'square-only-all-unrestricted',
-      'square-only-color-unrestricted',
-      'square-only-edge-sector-unrestricted',
-      'square-only-edge-unrestricted',
-    ],
-    [
-      'square-only-all',
-      'square-only-edge-sector',
-      'square-only-edge',
-    ]
-  );
-
-  await writeWith( 'general-edge',
-    [
-      'general-edge-unrestricted',
-      'square-only-edge-unrestricted',
-    ],
-    [
-      'general-edge',
-      'square-only-edge',
-    ]
-  );
-
-  await writeWith( 'general-color',
-    [
-      'general-color-unrestricted',
-      'square-only-color-unrestricted',
-    ],
-    [
-      // 'general-color', TODO: nope, BAD!
-      // 'square-only-color', TODO: nope, BAD!
-    ]
-  );
-
-  await writeWith( 'general-edge-sector',
-    [
-      'general-edge-sector-unrestricted',
-      'general-edge-unrestricted',
-      'square-only-edge-sector-unrestricted',
-      'square-only-edge-unrestricted',
-    ],
-    [
-      'general-edge-sector',
-      'general-edge',
-      'square-only-edge-sector',
-      'square-only-edge',
-    ]
-  );
-
-  await writeWith( 'general-all',
-    [
-      'general-all-unrestricted',
-      'general-color-unrestricted',
-      'general-edge-sector-unrestricted',
-      'general-edge-unrestricted',
-      'square-only-all-unrestricted',
-      'square-only-color-unrestricted',
-      'square-only-edge-sector-unrestricted',
-      'square-only-edge-unrestricted',
-    ],
-    [
-      'general-all',
-      'general-edge-sector',
-      'general-edge',
-      'square-only-all',
-      'square-only-edge-sector',
-      'square-only-edge',
-    ]
-  );
+        'general-edge-unrestricted',
+        'square-only-edge-unrestricted',
+      ],
+      [
+        'general-edge',
+        'square-only-edge',
+      ]
+    );
+  }
+  else if ( name === 'general-color' ) {
+    await writeWith( 'general-color',
+      [
+        'general-color-unrestricted',
+        'square-only-color-unrestricted',
+      ],
+      [
+        // 'general-color', TODO: nope, BAD!
+        // 'square-only-color', TODO: nope, BAD!
+      ]
+    );
+  }
+  else if ( name === 'general-edge-sector' ) {
+    await writeWith( 'general-edge-sector',
+      [
+        'general-edge-sector-unrestricted',
+        'general-edge-unrestricted',
+        'square-only-edge-sector-unrestricted',
+        'square-only-edge-unrestricted',
+      ],
+      [
+        'general-edge-sector',
+        'general-edge',
+        'square-only-edge-sector',
+        'square-only-edge',
+      ]
+    );
+  }
+  else if ( name === 'general-all' ) {
+    await writeWith( 'general-all',
+      [
+        'general-all-unrestricted',
+        'general-color-unrestricted',
+        'general-edge-sector-unrestricted',
+        'general-edge-unrestricted',
+        'square-only-all-unrestricted',
+        'square-only-color-unrestricted',
+        'square-only-edge-sector-unrestricted',
+        'square-only-edge-unrestricted',
+      ],
+      [
+        'general-all',
+        'general-edge-sector',
+        'general-edge',
+        'square-only-all',
+        'square-only-edge-sector',
+        'square-only-edge',
+      ]
+    );
+  }
+  else {
+    throw new Error( 'unknown name', name );
+  }
 
   await disposeBrowser( browser );
 } )();
