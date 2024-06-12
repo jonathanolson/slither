f484a21a62c5498125bd77a3a4b54cee67cbbc10
Jaybrainer <68438886+Jaybrainer@users.noreply.github.com>
2024-06-11 21:03:32 -0700

    Configure Prettier

---
+++ prettier.config.js
@@ -0,0 +1,19 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  experimentalTernaries: true,
+  printWidth: 80,
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
+
+export default config;
